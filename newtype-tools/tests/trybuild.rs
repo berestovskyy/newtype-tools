54a4bcf8b5b5837127b05454bb53458a19ecb1a2
Andriy Berestovskyy <berestovskyy@gmail.com>
2026-04-21 14:16:09 +0200

    test: refactor trybuild test (#41)

--- newtype-tools/tests/trybuild.rs
+++ newtype-tools/tests/trybuild.rs
@@ -3,5 +3,7 @@
 #[test]
 fn newtype_trybuild() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/trybuild/*.rs");
+    t.compile_fail("tests/trybuild/conversions/*.rs");
+    t.compile_fail("tests/trybuild/newtype/*.rs");
+    t.compile_fail("tests/trybuild/ops/*.rs");
 }
