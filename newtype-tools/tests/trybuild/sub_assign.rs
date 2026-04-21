791e9171c6ad2cfb6620df0f886c3440e30aa548
Andriy Berestovskyy <berestovskyy@gmail.com>
2026-04-21 13:59:08 +0200

    test: increase code coverage (#40)

---
+++ newtype-tools/tests/trybuild/sub_assign.rs
@@ -0,0 +1,33 @@
+/// `#[newtype(sub_assign(type, with = expr))]`
+
+mod missing_type {
+    #[derive(newtype_tools::Newtype)]
+    #[newtype(sub_assign())]
+    struct Oranges(u32);
+}
+
+mod missing_type_comma {
+    #[derive(newtype_tools::Newtype)]
+    #[newtype(sub_assign(u64))]
+    struct Oranges(u32);
+}
+
+mod missing_type_comma_with {
+    #[derive(newtype_tools::Newtype)]
+    #[newtype(sub_assign(u64,))]
+    struct Oranges(u32);
+}
+
+mod missing_type_comma_with_eq {
+    #[derive(newtype_tools::Newtype)]
+    #[newtype(sub_assign(u64, with))]
+    struct Oranges(u32);
+}
+
+mod missing_type_comma_with_eq_expr {
+    #[derive(newtype_tools::Newtype)]
+    #[newtype(sub_assign(u64, with =))]
+    struct Oranges(u32);
+}
+
+fn main() {}
